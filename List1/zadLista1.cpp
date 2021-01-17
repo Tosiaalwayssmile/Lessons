916c9a0dd225427cdaba0691145bd654b85f6e8a
Tosia <antoninamatuszek@gmail.com>
2021-01-17 14:07:08 +0100

    Modify exercise 1
    Finished exercise 1. Added fix for non-float input.

--- List1/zadLista1.cpp
+++ List1/zadLista1.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
+#include <ctype.h> //isdigit
+#include <sstream> //stringstream
 using namespace std;
 
 int main()
 {
-    float katS = 0;
-    int katR = 0;
-    cout << "Program przeliczajcy wielkość kąta wyrażoną w stopniach na radiany" << endl; 
+    /************************************************** ZADANIE 1 ***************************************************/
+    /************ POLISH DESCRIPTION: Program przeliczajcy wielkość kąta wyrażoną w stopniach na radiany ************/
     /*
-        Wzór na zamianę stopni na radiany to:
-        n(stopni) = n*pi/180 
+        Formula for converting degrees to radians:
+        n(degrees) = n*pi/180 
     */
-    cout << "Podaj wielkość kąta w stopniach: "; 
-    cin >> katS;
-    katR = katS*3,14/180;
-    cout << "\nWielkość podanego kąta w radianach wynosi: " << endl;
+    cout << "Ex.1 Program converting angles from degrees to radians" << endl; 
+    string degreeAngleString = "";
+    bool notNumberBool = true;
+    float degreeAngle = 0;
+    while(notNumberBool)
+    {
+        cout << "Insert angle in degrees: "; 
+        cin >> degreeAngleString;
+        if( degreeAngleString.find_first_not_of("1234567890.-") != string::npos ) 
+        {
+            cout << "invalid number: " << degreeAngleString << endl;
+        } 
+        else 
+        {
+            degreeAngle = stof(degreeAngleString);
+            notNumberBool = false;
+        }
+    }  
+    cout << "The radian measure of given angle equals: " << degreeAngle*3.14/180 << endl << endl;
+
+    /************************************************** ZADANIE 2 ***************************************************/
+    /*************************** POLISH DESCRIPTION: Program obliczający objętość stożka ***************************/
+
+    cout << "Ex.2 Work in progress, thanks for your patience" <<endl;
+
     return 0;
 
 }
