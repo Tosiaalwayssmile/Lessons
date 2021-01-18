9444c6dfda3e2caa0c862b7aacee82fdc94da05d
Tosia <antoninamatuszek@gmail.com>
2021-01-18 12:35:29 +0100

    Add ex. 1
    Finished exercise 1 from List2

--- List2/zadLista2.cpp
+++ List2/zadLista2.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
+/********************************************************* ZADANIE 1 **********************************************************/
+/**************** POLISH DESCRIPTION: Program, który wczytuje liczbę rzeczywistą i oblicza z niej pierwiastek. ****************/
+/********************************** Działanie powtarzane jest dopóki nie zostanie wczytane 0. *********************************/
+
+cout << "Program finding square root of a given integer number" << endl;
+int number = 0;
+do
+{   
+    cout << "Insert number: ";
+    cin >> number;
+    cout << "The square root of " +  std::to_string(number) + " is " << sqrt(number) << endl;
+} while (number != 0);
 
 
+
+/********************************************************* ZADANIE 2 **********************************************************/
+/**************** POLISH DESCRIPTION: Program, który wczytuje liczbę rzeczywistą i oblicza z niej pierwiastek. ****************/
+/********************************** Działanie powtarzane jest dopóki nie zostanie wczytane 0. *********************************/
 return 0;
 }
