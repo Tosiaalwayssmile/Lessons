234800456805d52a5f40336acaf12b85a7470bac
Tosia <antoninamatuszek@gmail.com>
2021-01-19 23:31:38 +0100

    Finish ex.3
    Finished ex.3, added inputValidation, it works of negative numbers.

---
+++ List2/inputValidation.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+std::string inputValidation(std::string type);
+
+std::string inputValidation(std::string type)
+{
+    std::string input = "null";
+    bool checkFlag = 0;
+    while (!checkFlag)
+    {
+        std::cin >> input;
+        if (type == "int" || type == "integer") 
+        {
+            if (input[0] == '-')
+            {
+                if (isdigit(input[1]))
+                {
+                    checkFlag = 1;
+                    std::cout << "Noice, you entered a valid value" << std::endl;
+                }
+                else
+                {
+                    std::cout << "Error. Invalid value, re-enter: ";
+                    }
+            }
+            else
+            {
+                if (isdigit(input[0]))
+                {
+                    checkFlag = 1;
+                    std::cout << "Noice, you entered a valid value" << std::endl;
+                }
+                else
+                {
+                    std::cout << "Error. Invalid value, re-enter: ";
+                    }
+            }
+        }
+        else if (type == "float") 
+        {
+            if (input[0] == '-')
+            {
+                if (isdigit(input[1]))
+                {
+                    checkFlag = 1;
+                    std::cout << "Good job, you entered a valid value" << std::endl;
+                }
+                else
+                {
+                    std::cout << "Error. Invalid value, re-enter: ";
+                    }
+            }
+            else
+            {
+                if (isdigit(input[0]))
+                {
+                    checkFlag = 1;
+                    std::cout << "Noice, you entered a valid value" << std::endl;
+                }
+                else
+                {
+                    std::cout << "Error. Invalid value, re-enter: ";
+                    }
+            }
+        }
+        else
+            std::cout << "Error in code, check function parameter when calling function" << std::endl;
+    }
+    return input;
+}
