a14ae9c759d62a225dd32111be3df53c50978e8b
Tosia <antoninamatuszek@gmail.com>
2021-01-22 18:33:59 +0100

    Finish List5

--- List5/ex1Functions.cpp
+++ List5/ex1Functions.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <algorithm>
 
 void fillIn(int lowest, int highest, int groupPoints[], int groupSize);
-void summary(int groupPoints[], int groupSize);
+void summary(int groupPoints[], int groupSize, int summaryPoints[]);
 float average(int groupPoints[], int groupSize);
+float median(int groupPoints[], int groupSize);
+void mode(int summaryPoints[], int pointsRange);
 
 void fillIn(int lowest, int highest, int groupPoints[], int groupSize)
 {
@@ -12,22 +15,18 @@ void fillIn(int lowest, int highest, int groupPoints[], int groupSize)
     }
 }
 
-void summary(int groupPoints[], int groupSize)
+void summary(int groupPoints[], int groupSize, int summaryPoints[])
 {   
-    int summary[11] = {0};
-    int counter = 0;
     for(int i = 0; i <= 10; i++)
     {
         for(int j = 0; j < groupSize; j++)
         {
             if (groupPoints[j] == i)
             {
-                counter++;
-                summary[i] = counter;
+                summaryPoints[i]++;
             }
         }
-        std::cout << i << " points gave: " << summary[i] << " people." << std::endl;
-        counter = 0;
+        std::cout << i << " points gave:  " << summaryPoints[i] << " people." << std::endl;
     }
     std::cout << std::endl;
 }
@@ -40,5 +39,67 @@ float average(int groupPoints[], int groupSize)
     {
         sum += groupPoints[i];
     }
-    return sum / groupSize;
+    return (float)sum / (float)groupSize;       //Need to first cast integers to float before doing the calculation
+}
+
+
+float median(int groupPoints[], int groupSize)
+{   
+
+    std::sort(groupPoints, groupPoints + groupSize);        // sort from lowest to highest
+
+    if (groupSize % 2)      //if middle value is median
+    {
+        return groupPoints[groupSize / 2];      //return median AKA value in the middle of sorted array
+    }
+    
+    else        // average of two middle values equals median
+    {
+        return (groupPoints[groupSize / 2] + groupPoints[groupSize / 2 + 1]) / 2;
+    }
+}
+
+template <typename T> void displayTable(T table[], int arrSize)
+{
+    for (int i = 0; i < arrSize; i++)
+    {
+        std::cout << table[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void mode(int summaryPoints[], int pointsRange)
+{   
+    int mode = 0;
+    for (int i = 1; i < pointsRange; i++)
+    {
+        if(summaryPoints[i] > summaryPoints[mode])
+        {
+            mode = i;
+        }
+    }
+    for (int i = 0; i < pointsRange; i++)
+    {
+        if(summaryPoints[i] == summaryPoints[mode])
+        {
+            std::cout << "Mode equals: " << i << std::endl;
+        }
+    }
+}
+
+void histogram(int summaryPoints[], int pointsRange)
+{   
+    for (int i = 0; i < pointsRange; i++)
+    {
+        std::cout << "\n" << i << " points  ";
+        if(i == 10)
+            {
+                std::cout << "\b";
+            }
+        while(summaryPoints[i])
+        {
+            std::cout << "#";
+            summaryPoints[i]--;
+        }
+    }
 }
--- List5/zadLista5.cpp
+++ List5/zadLista5.cpp
@@ -3,6 +3,7 @@
 #include "ex1Functions.cpp"
 
 #define groupSize 100
+#define pointsRange 11
 
 int main()
 {
@@ -12,7 +13,6 @@ int main()
     2. Wylosować elementy tej tablicy z przedziału <0, 10>.
     3. Podsumować wyniki głosowania tzn. obliczyć ile osób przyznało 0 punktów, 1 punkt, 2 punkty itp.
     4. Obliczyć średnią liczbę punktów.
-    ####DONE#####
     5. Obliczyć medianę (środkowa wartość w posortowanej tablicy, w przypadku parzystej
     liczby elementów tablicy jest to średnia dwóch środkowych wartości).
     6. Obliczyć dominantę (najczęściej występującą wartość, w przypadku kilku
@@ -22,17 +22,30 @@ int main()
     Program podzielić na funkcje. Rozmiar tablicy ustalić za pomocą dyrektywy definiującej. */
 
     std::cout << "*******************************************************************" 
-                 "\nEx.1 Program analyzing data from array filled with random numbers." 
+                 "\nEx.1 Group of people rated a movie from 0-10." 
                  "\n******************************************************************" << std::endl;
 
     int groupPoints[groupSize];
+
     srand(time(NULL));
     fillIn(0, 10, groupPoints, groupSize);
 
-    summary(groupPoints, groupSize);
+    //displayTable<int>(groupPoints, groupSize);        // Display to see unsorted table
+
+    int summaryPoints[pointsRange] = {0};
+    summary(groupPoints, groupSize, summaryPoints);
     
     std::cout << "Average of given points equals: " << average(groupPoints, groupSize) << "." << std::endl;
     
-    std::cout << "Given points: " << std::endl;
+    std::cout << "Median of given points equals: " << median(groupPoints, groupSize) << "." << std::endl;
+    
+    //displayTable<int>(groupPoints, groupSize);        //Display to see sorted table (function median calls sort function)
+
+    mode(summaryPoints, pointsRange);    //mode in polish means dominanta
+
+    std::cout << "\n    Histogram";
+    histogram(summaryPoints, pointsRange);
+    
+    std::cout << "\nThanks for stopping by :)" << std::endl;
     return 0;
 }
